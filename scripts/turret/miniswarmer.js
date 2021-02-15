24f7f642ea3d1263bdc41c96aea1865adafc21ed
guiYMOUR <77377005+guiYMOUR@users.noreply.github.com>
2021-02-15 11:33:56 +0800

    Miniswarmer can shoot graphite.

--- scripts/turret/miniswarmer.js
+++ scripts/turret/miniswarmer.js
@@ -1,5 +1,17 @@
 //
 const lib = require("blib");
+const m = extend(MissileBulletType,{});
+m.speed = 3.5;
+m.damage = 10;
+m.width = 7;
+m.height = 8;
+m.shrinkY = 0;
+m.drag = -0.01;
+m.homingPower = 0.08;
+m.splashDamageRadius = 25;
+m.splashDamage = 15;
+m.hitEffect = Fx.blastExplosion;
+
 const swMin = extendContent(ItemTurret, "mini-swarmer", {});
 lib.setBuildingSimple(swMin, ItemTurret.ItemTurretBuild, {});
 swMin.reloadTime = 35;
@@ -9,8 +21,9 @@ swMin.inaccuracy = 10;
 swMin.range = 190;
 swMin.xRand = 4;
 swMin.size = 2;
-swMin.health = 500 * 2 * 2;
+swMin.health = 200 * 2 * 2;
 swMin.ammo(
+    Items.graphite, m,
     Items.blastCompound, Bullets.missileExplosive,
             Items.pyratite, Bullets.missileIncendiary,
             Items.surgeAlloy, Bullets.missileSurge
