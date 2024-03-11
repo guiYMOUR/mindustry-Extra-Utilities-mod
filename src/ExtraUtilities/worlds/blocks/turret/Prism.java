87fb2bf95dda14849a50c84ed1c9020646ed1312
guiY <77377005+guiYMOUR@users.noreply.github.com>
2024-03-11 15:14:46 +0800

    1.1.5.6

--- src/ExtraUtilities/worlds/blocks/turret/Prism.java
+++ src/ExtraUtilities/worlds/blocks/turret/Prism.java
@@ -1,49 +1,66 @@
 package ExtraUtilities.worlds.blocks.turret;
 
-import ExtraUtilities.content.EUSounds;
-import arc.Core;
+import ExtraUtilities.content.EUStatusEffects;
+import ExtraUtilities.worlds.entity.bullet.PrismLaser;
 import arc.math.Angles;
 import arc.math.Mathf;
 import arc.math.geom.Vec2;
 import arc.struct.ObjectMap;
-import arc.struct.Seq;
 import arc.util.Time;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
-import mindustry.Vars;
+import mindustry.content.Liquids;
+import mindustry.content.StatusEffects;
 import mindustry.entities.bullet.BulletType;
 import mindustry.gen.Bullet;
 import mindustry.gen.Sounds;
-import mindustry.type.Liquid;
-import mindustry.world.blocks.defense.turrets.LaserTurret;
-import mindustry.world.blocks.defense.turrets.PowerTurret;
+import mindustry.type.StatusEffect;
+import mindustry.world.blocks.defense.turrets.Turret;
+import mindustry.world.consumers.Consume;
+import mindustry.world.consumers.ConsumeLiquid;
 import mindustry.world.consumers.ConsumeLiquidBase;
 import mindustry.world.meta.Stat;
-import mindustry.world.meta.StatUnit;
 import mindustry.world.meta.StatValues;
 
 import java.util.Arrays;
 
 import static mindustry.Vars.*;
 
-public class Prism extends PowerTurret {
+/**
+ * @author guiY
+ * redo 2024 0307
+ * 重新做了什么？持续发射持续消耗；子弹显示子弹效果子弹伤害
+ * */
+
+public class Prism extends Turret {
     //三棱镜光线聚集的强度
     public float angleShiftStrength = 5;
     //角度
     public float shiftAngel = 30;
     //初始散射和最后的交替散射
     public float sideOffsetFirst = 4;
-    public float sideOffset = 2.5f;
+    public float sideOffset = 3f;
     //激光粗度和交替变化速度，<1
     public float fade = 0.001f;
     //纯交替速度（越大越慢
     public int upTime = 3;
-    //射击间隔
-    public float shootDuration = 60*10;
     //烎移动炮台速度
-    public float firingMoveFract = 0.25f;
-    //reload还要注释?
-    public float reloadTime = 240;
+    public float firingMoveFract = 0.4f;
+    //还原泰拉瑞亚棱镜的射击最高持续时间
+    public float maxShootDr = 60 * 60f;
+
+    public StatusEffect[] statuses = {
+            StatusEffects.blasted,
+            StatusEffects.shocked,
+            EUStatusEffects.poison,
+            StatusEffects.freezing,
+            StatusEffects.wet,
+            StatusEffects.sapped
+    };
+    public BulletType shootType;
+    public BulletType[] shootTypes = {};
+    public float rainbowDamage = 90;
+    public float rainbowWidth = 11;
 
     public Prism(String name) {
         super(name);
@@ -52,13 +69,28 @@ public class Prism extends PowerTurret {
         shootSound = Sounds.laser;//EUSounds.prismShoot;
         loopSound = Sounds.pulse;//EUSounds.prismLoop;
         loopSoundVolume = 2f;
+        recoilTime = 30f;
+
+        consumeLiquid(Liquids.cryofluid, 30/60f);
+        liquidCapacity = 30f;
     }
 
     @Override
     public void init() {
         super.init();
-        if (this.coolant == null) {
-            this.coolant = this.findConsumer((c) -> c instanceof ConsumeLiquidBase);
+        if(statuses.length > 0) {
+            shootTypes = new BulletType[statuses.length];
+
+            for (int i = 0; i < statuses.length; i++) {
+                int finalI = i;
+                shootTypes[i] = new PrismLaser(rainbowDamage, rainbowWidth, range){{
+                    status = statuses[finalI];
+                    statusDuration = 60f;
+
+                    if(finalI < 3) pierceArmor = true;
+                }};
+            }
+            shootType = shootTypes[0];
         }
     }
 
@@ -66,85 +98,97 @@ public class Prism extends PowerTurret {
     public void setStats() {
         super.setStats();
         this.stats.remove(Stat.reload);
-        this.stats.add(Stat.reload, 60f / this.reloadTime + " * 6", StatUnit.perSecond);
-        this.stats.remove(Stat.booster);
-        this.stats.add(Stat.input, StatValues.boosters(reloadTime, coolant.amount, coolantMultiplier, false, this::consumesLiquid));
-        this.stats.remove(Stat.ammo);
-        stats.add(Stat.ammo, StatValues.ammo(ObjectMap.of(this, shootType)));
+        this.stats.remove(Stat.inaccuracy);
+
+        for(int i = 0; i < shootTypes.length; i++) {
+            this.stats.add(Stat.ammo, StatValues.ammo(ObjectMap.of(new Object[]{statuses[i], shootTypes[i]})));
+        }
     }
 
-    public class PrismBuild extends PowerTurretBuild{
-        public Bullet[] bullets = new Bullet[]{null, null, null, null, null, null};
-        //public Seq<Bullet> bullets = new Seq<>(6);
+    public class PrismBuild extends TurretBuild{
+        public Bullet[] bullets = new Bullet[statuses.length];
 
-        public float bulletLife = 0f;
         public float bulletTime = 0f;
         public float bulletHeat = 0f;
         public float faded = fade;
-        public float _reloadTime = reloadTime;
+        public float bulletLife = 0f;
 
         public Vec2 tr = new Vec2();
 
+        public BulletType useAmmo() {
+            return Prism.this.shootType;
+        }
+
+        public boolean hasAmmo() {
+            return canConsume();
+        }
+
+        private boolean liquidAcc(){
+            return !hasLiquids || liquids.get(liquids.current()) > liquidCapacity/6;
+        }
+
         @Override
-        protected void updateCooling() {
+        public boolean canConsume() {
+            return super.canConsume() && liquidAcc();
+        }
+
+        public BulletType peekAmmo() {
+            return Prism.this.shootType;
         }
 
         @Override
         public boolean shouldConsume() {
-            return bulletLife>0 || this.isActive() || this.isShooting();
+            return isActive() || isShooting() && liquidAcc();
         }
 
-        public boolean checkBullet(Bullet[] bs){
-            if(bs.length == 0) return false;
-            for (Bullet b : bs) {
+        public boolean checkBullet(){
+            if(shootTypes.length == 0 || bullets.length == 0) return false;
+            for (Bullet b : bullets) {
                 if (b == null) return false;
             }
             return true;
         }
 
+        public void removeBullet(){
+            for(int i = 0; i < bullets.length; i++){
+                if(bullets[i] == null) continue;
+                if(!bullets[i].isAdded() || bullets[i].type == null || bullets[i].owner != this) bullets[i] = null;
+            }
+        }
+
         @Override
         public void updateTile() {
             super.updateTile();
-            float used;
-            if(bulletLife > 0 && checkBullet(bullets)){
+            float ammoFact = this.efficiency;
+            Consume cs = Prism.this.findConsumer((f) -> f instanceof ConsumeLiquidBase);
+            if (cs instanceof ConsumeLiquid cl) {
+                ammoFact = Math.min(ammoFact, this.liquids.get(cl.liquid) / Prism.this.liquidCapacity);
+            }
+
+            this.unit.ammo((float)this.unit.type().ammoCapacity * ammoFact);
+
+            removeBullet();
+            if(bulletLife < maxShootDr && checkBullet()){
                 wasShooting = true;
                 faded = Math.min(faded + fade * Time.delta, 1f);
                 for(int n = 0; n < bullets.length; n++){
                     int data = (n * (360 / bullets.length));
-                    float sine = Mathf.sinDeg(data + (bulletTime * (angleShiftStrength * (1.6f-bulletHeat))));
-                    //float sine = Mathf.sinDeg(data + (bulletTime * angleShiftStrength * faded));
-                    tr.trns(rotation, (block.size * tilesize / 2f) - recoil + shootX, sine * (sideOffsetFirst * bulletHeat + sideOffset));
-                    bullets[n].rotation(rotation + ((sine * shiftAngel) * bulletHeat));
+                    float sin = Mathf.sinDeg(data + (bulletTime * (angleShiftStrength * (1.6f - bulletHeat))));
+                    tr.trns(rotation, (block.size * tilesize / 2f) - recoil + shootX, sin * (sideOffsetFirst * bulletHeat + sideOffset));
+                    bullets[n].rotation(rotation + ((sin * shiftAngel) * bulletHeat));
                     bullets[n].set(x + tr.x, y + tr.y);
-                    bullets[n].time = 0;
+                    //Mana Sickness
+                    bullets[n].damage = rainbowDamage * (1 - (bulletLife/maxShootDr) * 0.75f);
+                    //need half -> half
+                    //bullets[n].damage = rainbowDamage * Math.max(1 - bulletLife/maxShootDr, 0.5f);
+                    if(isShooting() && hasAmmo()) bullets[n].time = 0;
                 }
+                if(isShooting() && hasAmmo()) bulletLife += Time.delta;
                 curRecoil = 1f;
                 heat = 1f;
                 bulletTime += Time.delta;
-                bulletLife -= Time.delta / Math.max(power.status, 0.00001f);
-                if (bulletLife <= 0) {
-                    Arrays.fill(bullets, null);
-                    bulletTime = 0;
-                    bulletHeat = 0;
-                    faded = fade;
-                }
-            } else if(_reloadTime > 0){
-                wasShooting = true;
-                if (coolant != null) {
-                    Liquid liquid = liquids.current();
-                    float maxUsed = coolant.amount;
-                    used = (cheating() ? maxUsed : Math.min(liquids.get(liquid), maxUsed)) * delta();
-                    _reloadTime -= used * liquid.heatCapacity * coolantMultiplier;
-                    liquids.remove(liquid, used);
-                    if (Mathf.chance(0.06d * (double)used)) {
-                        coolEffect.at(x + Mathf.range((float)(size * 8) / 2f), y + Mathf.range((float)(size * 8) / 2f));
-                    }
-                } else {
-                    _reloadTime -= this.edelta();
-                }
             }
             bulletHeat = Mathf.lerpDelta(bulletHeat, 0, (float) Math.pow(faded, upTime));
-            //bulletHeat = Mathf.lerpDelta(bulletHeat, 0, fade);
         }
 
         @Override
@@ -153,61 +197,45 @@ public class Prism extends PowerTurret {
 
         @Override
         protected void updateShooting() {
-            if(bulletLife > 0 || bullets[0] != null){
-                return;
-            }
-            if(_reloadTime <= 0 && (power.status > 0 || cheating())){
-                BulletType type = peekAmmo();
-                shoot(type);
-                _reloadTime = reloadTime;
+            if(!checkBullet()) {
+                if (canConsume() && !charging() && shootWarmup >= Prism.this.minWarmup) {
+                    shoot();
+                }
             }
         }
 
         @Override
-        protected void shoot(BulletType type) {
-            tr.trns(rotation, shootX, 0);
-            bullet(type, rotation);
+        protected void updateCooling() {
         }
 
-        protected void bullet(BulletType type, float angle){
+        protected void shoot() {
+            tr.trns(rotation, shootX, 0);
+            bullet(rotation);
+        }
+
+        protected void bullet(float angle){
+            Arrays.fill(bullets, null);
             bulletTime = 0;
-            bulletLife = shootDuration;
-            for(int s = 0; s < 6; s++){
-                int data = (s * (360 / 6));
+            bulletHeat = 0;
+            bulletLife  = 0;
+            faded = fade;
+            for(int s = 0; s < shootTypes.length; s++){
+                if(shootTypes[s] == null) return;
+                int data = (s * (360 / shootTypes.length));
                 float sine = Mathf.sinDeg(data + (bulletTime * angleShiftStrength));
                 tr.trns(angle, block.size * tilesize / 2f, sine * sideOffset);
-                bullets[s] = type.create(tile.build, team, x + tr.x, y + tr.y, angle + (sine * shiftAngel));
+                bullets[s] = shootTypes[s].create(tile.build, team, x + tr.x, y + tr.y, angle + (sine * shiftAngel));
                 bullets[s].data = data;
             }
             bulletHeat = 1;
         }
 
-        public float progress() {
-            return 1f - Mathf.clamp(_reloadTime / reloadTime);
-        }
-
         protected void turnToTarget(float targetRot) {
-            this.rotation = Angles.moveToward(this.rotation, targetRot, power.status * Prism.this.rotateSpeed * this.delta() * (bulletLife > 0 ? Prism.this.firingMoveFract : 1.0F));
-        }
-
-        public float activeSoundVolume() {
-            return 1f;
+            this.rotation = Angles.moveToward(this.rotation, targetRot, power.status * Prism.this.rotateSpeed * this.delta() * (checkBullet() ? Prism.this.firingMoveFract : 1.0F));
         }
 
         public boolean shouldActiveSound() {
             return bullets[0] != null;
-        }
-
-        @Override
-        public void write(Writes write) {
-            super.write(write);
-            write.f(_reloadTime);
-        }
-
-        @Override
-        public void read(Reads read, byte revision) {
-            super.read(read, revision);
-            _reloadTime = read.f();
         }
     }
 }
