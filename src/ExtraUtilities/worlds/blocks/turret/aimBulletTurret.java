1b214d74bd5ec412304eca094e8a331d896de17c
guiY <77377005+guiYMOUR@users.noreply.github.com>
2024-05-13 22:07:37 +0800

    1.1.6.1

--- src/ExtraUtilities/worlds/blocks/turret/aimBulletTurret.java
+++ src/ExtraUtilities/worlds/blocks/turret/aimBulletTurret.java
@@ -6,10 +6,14 @@ import arc.math.Mathf;
 import arc.util.Time;
 import mindustry.entities.bullet.BulletType;
 import mindustry.gen.Bullet;
+import mindustry.gen.Unit;
+import mindustry.logic.LAccess;
 import mindustry.world.blocks.defense.turrets.ItemTurret;
+import mindustry.world.blocks.logic.LogicBlock;
 
 public class aimBulletTurret extends ItemTurret {
     public BulletType aimBullet;
+    public boolean logicCtrlAble = false;
 
     public aimBulletTurret(String name) {
         super(name);
@@ -43,6 +47,22 @@ public class aimBulletTurret extends ItemTurret {
                 useAmmo();
             }
 
+        }
+
+        @Override
+        public void control(LAccess type, double p1, double p2, double p3, double p4) {
+            if(logicCtrlAble) super.control(type,p1, p2, p3, p4);
+            if (type == LAccess.enabled) {
+                enabled = !Mathf.zero((float)p1);
+            }
+        }
+
+        @Override
+        public void control(LAccess type, Object p1, double p2, double p3, double p4) {
+            if(logicCtrlAble) super.control(type,p1, p2, p3, p4);
+            if (type == LAccess.config && block.logicConfigurable && !(p1 instanceof LogicBlock.LogicBuild)) {
+                configured(null, p1);
+            }
         }
     }
 }
