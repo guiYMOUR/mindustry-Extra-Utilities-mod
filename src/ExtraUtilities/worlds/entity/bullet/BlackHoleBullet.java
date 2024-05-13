ad2ab84fb8af70eb1c80b85c56e4f4034e123ba2
guiY <77377005+guiYMOUR@users.noreply.github.com>
2024-05-13 22:09:14 +0800

    1.1.6.1

--- src/ExtraUtilities/worlds/entity/bullet/BlackHoleBullet.java
+++ src/ExtraUtilities/worlds/entity/bullet/BlackHoleBullet.java
@@ -6,6 +6,7 @@ import arc.Core;
 import arc.graphics.Color;
 import arc.math.Interp;
 import arc.math.Mathf;
+import arc.math.geom.Vec2;
 import arc.util.Time;
 import arc.util.Tmp;
 import arc.util.pooling.Pools;
@@ -14,6 +15,8 @@ import mindustry.content.Fx;
 import mindustry.entities.Units;
 import mindustry.entities.bullet.BulletType;
 import mindustry.gen.Bullet;
+import mindustry.gen.Groups;
+import mindustry.type.unit.MissileUnitType;
 
 public class BlackHoleBullet extends BulletType {
     public float inRad, outRad, rotateSpeed;
@@ -21,9 +24,10 @@ public class BlackHoleBullet extends BulletType {
     public float minWidth = 0.9f, maxWidth = 2.2f;
     public float minSpeed = 0.5f, midSpeed = 0.7f, maxSpeed = 1.8f;
 
-    //引力
-    public float impulse = 1200;
+    public float impulse = 6.67f * 1e-1f;
 
+    //吸积盘颜色
+    public Color accColor = Color.valueOf("B778FF");
     //吸积盘基数
     public int amount = 2;
 
@@ -62,13 +66,24 @@ public class BlackHoleBullet extends BulletType {
 
         float finalIn = in;
         Units.nearbyEnemies(b.team, b.x, b.y, outRad * 2, e -> {
-            if(e.targetable(b.team)) {
-                if (e.within(b, outRad)) {
-                    float p = percent(b.x, b.y, e.x, e.y, outRad);
-                    e.impulseNet(Tmp.v3.set(e).sub(b.x, b.y).nor().scl(-(impulse * p * Time.delta) * finalIn));
-                } else {
-                    float p = percent(b.x, b.y, e.x, e.y, outRad * 2);
-                    e.impulseNet(Tmp.v3.set(e).sub(b.x, b.y).nor().scl(-(impulse / 2 * p * Time.delta) * finalIn));
+            if(e != null && e.targetable(b.team)) {
+                float p = F(b.x, b.y, e.x, e.y, impulse * Math.abs(e.mass() + 1f), outRad * 2);
+                if(e.type instanceof MissileUnitType) p = F(b.x, b.y, e.x, e.y, impulse * 2000, outRad * 2);
+                e.impulseNet(Tmp.v3.set(e).sub(b).nor().scl(-p * Time.delta * finalIn));
+
+                e.vel.limit(5);
+            }
+        });
+
+        Groups.bullet.intersect(b.x - outRad * 2, b.y - outRad * 2, outRad * 4, outRad * 4, bullet -> {
+            if(bullet != null && bullet.within(b, outRad * 2) && bullet.team != b.team && bullet.type != null && (bullet.type.absorbable || bullet.type.hittable)){
+                float p = F(b.x, b.y, bullet.x, bullet.y, impulse * 10, outRad * 2);
+                Vec2 v = Tmp.v4.set(bullet).sub(b).nor().scl(-p * Time.delta * finalIn);
+                bullet.vel.add(v.x, v.y);
+                bullet.vel.limit(5);
+                if(bullet.within(b, inRad)) {
+                    bullet.damage = 0;
+                    bullet.remove();
                 }
             }
         });
@@ -86,13 +101,13 @@ public class BlackHoleBullet extends BulletType {
             EUFx.AccretionDiskEffect.at(
                     b.x,
                     b.y,
-                    0, Color.valueOf("B778FF"), data);
+                    0, accColor, data);
         }
     }
 
-    public float percent(float x, float y, float tx, float ty, float radius) {
+    public float F(float x, float y, float tx, float ty, float G, float r) {
         float dst = Mathf.dst(x, y, tx, ty);
-        float falloff = 0.2f;
-        return Mathf.lerp(1 - dst / radius, 1, falloff);
+        float ptr = 1 - dst/r;
+        return G * ptr;
     }
 }
