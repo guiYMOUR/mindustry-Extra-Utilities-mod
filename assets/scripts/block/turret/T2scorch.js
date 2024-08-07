47ebcda88a333f51c9a3ceb0b3be288c8621f317
guiY <77377005+guiYMOUR@users.noreply.github.com>
2024-08-07 13:58:42 +0800

    1.1.6.4

--- assets/scripts/block/turret/T2scorch.js
+++ assets/scripts/block/turret/T2scorch.js
@@ -6,22 +6,33 @@ const lib = require("blib");
 //const bullets = require("other/bullets");
 const items = require("game/items");
 const FlameBulletType = lib.getClass("ExtraUtilities.worlds.entity.bullet.FlameBulletType");
+const bullets = lib.getClass("ExtraUtilities.content.EUBulletTypes");
+const value = lib.getClass("ExtraUtilities.worlds.meta.EUStatValues");
 
-const T2scorch = new ItemTurret("T2scorch");
+//const T2scorch = new ItemTurret("T2scorch");
+const T2scorch = extend(ItemTurret, "T2scorch", {
+    setStats(){
+        this.super$setStats();
+        if(this.ammoTypes == null) return;
+
+        this.stats.add(Stat.ammo, value.ammoString(this.ammoTypes));
+    },
+});
 T2scorch.ammo(
-    Items.coal, new FlameBulletType(Pal.lightFlame, Pal.darkFlame, Color.gray, T2scorch.range+8, 14, 60, 22),
+    Items.coal, new FlameBulletType(Pal.lightFlame, Pal.darkFlame, Color.gray, T2scorch.range + 8, 14, 60, 22),
     Items.pyratite, (() => {
-        const b = new FlameBulletType(Pal.lightPyraFlame, Pal.darkPyraFlame, Color.gray, T2scorch.range+8, 20, 72, 22);
-        b.damage = 39;
+        const b = new FlameBulletType(Pal.lightPyraFlame, Pal.darkPyraFlame, Color.gray, T2scorch.range + 8, 20, 72, 22);
+        b.damage = 99;
         b.statusDuration = 60 * 6;
         return b;
     })(),
     Items.sporePod, (() => {
-        const b = new FlameBulletType(Pal.lightFlame, Pal.sapBulletBack, Color.gray, T2scorch.range+8, 25, 60, 25);
-        b.damage = 10;
+        const b = new FlameBulletType(Pal.lightFlame, Pal.sapBulletBack, Color.gray, T2scorch.range + 8, 25, 60, 25);
+        b.damage = 35;
         b.status = StatusEffects.sapped;
         return b;
-    })()
+    })(),
+    Items.blastCompound, bullets.expFlame(T2scorch.range, 52),
 );
 Object.assign(T2scorch, {
     size : 2,
@@ -35,7 +46,7 @@ Object.assign(T2scorch, {
     health : 400 * 2 * 2,
     shootSound : Sounds.flame,
 });
-T2scorch.coolant = T2scorch.consumeCoolant(0.2);
+lib.Coolant(T2scorch, 0.2, 2);
 T2scorch.requirements = ItemStack.with(
     Items.copper, 60,
     Items.graphite, 40,
