e6f26142c2b432b325fa95e6941d9cbfc201c7dc
guiY <77377005+guiYMOUR@users.noreply.github.com>
2024-11-15 15:24:52 +0800

    BE adapted

--- assets/scripts/blib.js
+++ assets/scripts/blib.js
@@ -6,7 +6,6 @@
 //星球设置区块
 
 //读取我自己的Java class
-const {aModName} = require("./blib");
 const urlLoader = Packages.java.net.URLClassLoader([Vars.mods.getMod(modName).file.file().toURI().toURL()], Vars.mods.mainLoader());
 exports.getClass = function (name){
     return Packages.rhino.NativeJavaClass(Vars.mods.scripts.scope, urlLoader.loadClass(name));
